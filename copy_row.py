4ae9c712a486ecd5eaac59c7bb5094522ef00fe5
a-flatt <adamjplatt@outlook.com>
2019-07-30 16:51:37 +0800

    Update copy_row.py

--- copy_row.py
+++ copy_row.py
@@ -1,11 +1,15 @@
 import openpyxl
 from openpyxl.utils import get_column_letter
 
-def copy_row(sheet, src_row, tgt_row):
+def copy_row_to_rows(sheet, start_row, finish_row, src_row):
+
+    for row in range(start_row, finish_row+1):
+        copy_row(sheet, row, src_row)
+
+def copy_row(sheet, tgt_row, src_row):
 
     tgt_range = '{}:{}'.format(tgt_row, tgt_row)
-    # src_range = '{}:{}'.format(src_row)
 
-    for col in range(1, len([cell._style for cell in sheet[tgt_range]])):
+    for col in range(1, num_styled_cols(sheet, tgt_range)):
         col_letter = get_column_letter(col)
         sheet['{}{}'.format(col_letter, tgt_row)]._style = sheet['{}{}'.format(col_letter, src_row)]._style
